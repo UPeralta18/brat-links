c901cfee9a2f266ce6b6e65f0d8b6a473cc66af1
UPeralta18 <uperalta18@gmail.com>
2023-10-18 09:19:52 -0300

    Transforming each line in a link

--- scripts/main.js
+++ scripts/main.js
@@ -1,11 +1,12 @@
 fetch("links.txt").then((res) => res.text()).then((text) => {
     var linksBox = document.getElementById('links');
-    const lines = text.split('\n');
+    var lines = text.split('\n');
+    var content = '';
     for (let i = 0; i < lines.length; i++) {
-        // Processar cada linha
-        // const line = document.createElement('p');
-        // line.textContent = lines[i];
-        // fileContents.appendChild(line);
-        console.log(lines[i]);
+        let text_link = lines[i].split(';');
+        content += '<a href="' + text_link[1] + '" target="_blank">' +
+                        '<li>' + text_link[0] + '</li>' +
+                    '</a>'
     }
+    linksBox.innerHTML = content;
 }).catch((e) => console.error(e));
